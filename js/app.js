3875d7d9787fcbc0b6a2312c9f489a54ed9b5a82
Owlman77 <jeanmarc.mcghee@gmail.com>
2016-12-09 17:09:57 +0000

    further planning

--- js/app.js
+++ js/app.js
@@ -6,17 +6,21 @@ console.log('linktest');
 // When end screen is shown display the score and game over.
 // Have the computer make a 4*4 square of 4 buttons( li elements) in a div
 // Have the computer make a random sequence of buttons light up for the player to click. Make sure they can't click it while this happens.
+// When play clicks a button a specifc sound (for each button) will play.
 // Only accept the answer when the sequence length entered = the sequence length shown
-// If player is correct then add +10 to the time and to the score.
-// If player is wrong then add -10 to time.
-// Then have the computer repeat do the same sequence but with a new random button press.
-// repeat until the user can no longer remember and runs out of time.
+// IF player is correct then add +10 seconds to the time and 10 points to the score.
+// IF player is wrong then add -10 seconds to time and -10 points from the score.
+// IF player is correct then have the computer repeat do the same sequence but with a new random button press.
+// IF player is incorrect then have the computer repeat the previous sequence.
+// Repeat until the user can no longer remember and runs out of time.
 
 
 
 var bomb = bomb || {};
 bomb.start;
-
+bomb.comSeq = [];
+bomb.pSeq = [];
+bomb.timer;
 document.addEventListener('DOMContentLoaded', bomb.start);
 
 
