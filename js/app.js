4091249f6128126351f2618154ffe05a48926017
Owlman77 <jeanmarc.mcghee@gmail.com>
2016-12-09 16:41:43 +0000

    intial planning and added Jquery link

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 console.log('linktest');
+
+// Have a timer that counts down from a predetermined time (1min)
+// Have a score box with a intial value of 0
+// When time runs out, clear the screen and make it go bang!
+// When end screen is shown display the score and game over.
+// Have the computer make a 4*4 square of 4 buttons( li elements) in a div
+// Have the computer make a random sequence of buttons light up for the player to click. Make sure they can't click it while this happens.
+// Only accept the answer when the sequence length entered = the sequence length shown
+// If player is correct then add +10 to the time and to the score.
+// If player is wrong then add -10 to time.
+// Then have the computer repeat do the same sequence but with a new random button press.
+// repeat until the user can no longer remember and runs out of time.
+
+
+
+var bomb = bomb || {};
+bomb.start;
+
+document.addEventListener('DOMContentLoaded', bomb.start);
+
+
+
+bomb.start = function start(){
+
+};
